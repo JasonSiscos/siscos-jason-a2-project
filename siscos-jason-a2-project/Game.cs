eba768e8377b662fcc7a94ccd771b8a8b1040521
jason <jason.siscos@mohawkcollege.ca>
2026-02-25 21:46:54 -0500

    change background to red

--- siscos-jason-a2-project/Game.cs
+++ siscos-jason-a2-project/Game.cs
@@ -26,7 +26,7 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
-            Window.ClearBackground(Color.Yellow);
+            Window.ClearBackground(Color.Red);
 
             Draw.FillColor = Color.White;
             for (int i = 0; i < 5; i++)
